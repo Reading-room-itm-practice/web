f730fb889dc95b0b7e3eadf5b98f4abc2f4928a3
unknown <krystianduda64@gmail.com>
2021-06-30 09:48:27 +0200

    Added dependiecies

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import router from './router'
 import store from './store'
 import axiosConfig from './axiosConfig'
 import ElementUI from 'element-ui'
+import Notifications from 'vue-notification'
+import VueI18n from 'vue-i18n'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 Vue.use(axiosConfig)
+Vue.use(Notifications)
+Vue.use(VueI18n)
 
 new Vue({
   router,
