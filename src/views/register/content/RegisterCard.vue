74ec9b7cc832156f99ff040b43096787b3a41752
unknown <krystianduda64@gmail.com>
2021-07-02 12:23:24 +0200

    Added post request for register

--- src/views/register/content/RegisterCard.vue
+++ src/views/register/content/RegisterCard.vue
@@ -2,6 +2,9 @@
   <el-row :gutter='24' align='top' justify='center' id='register'>
     <el-col :offset='14' :span='12'>
       <el-form :model='registerForm' :rules='validationRules' ref='registerForm'>
+        <el-form-item prop='username' label='Username'>
+          <email-form v-on:form-input='updateForm($event)' type='username'></email-form>
+        </el-form-item>
         <el-form-item prop='email' label='Email'>
           <email-form v-on:form-input='updateForm($event)' type='password'></email-form>
         </el-form-item>
@@ -24,8 +27,9 @@ import { Vue, Component } from 'vue-property-decorator'
 import EmailForm from '@/components/forms/EmailForm.vue'
 import PasswordForm from '@/components/forms/PasswordForm.vue'
 import PasswordConfirmationForm from '@/components/forms/PasswordConfirmationForm.vue'
-import { email, password, passwordConfirmation } from '@/components/validations/validationRules.ts'
+import { email, password, passwordConfirmation, username } from '@/components/validations/validationRules.ts'
 import { baseValidationRule } from '@/components/validations/baseValidationRule'
+import axios from 'axios'
 
 @Component({
   components: {
@@ -36,12 +40,14 @@ import { baseValidationRule } from '@/components/validations/baseValidationRule'
 })
 export default class RegisterCard extends Vue {
   public validationRules: baseValidationRule = {
+    username: username,
     email: email,
     password: password,
     passwordConfirmation: passwordConfirmation
   }
 
   private registerForm: Array<string> = {
+    username: '',
     email: '',
     password: '',
     passwordConfirmation: ''
@@ -54,7 +60,9 @@ export default class RegisterCard extends Vue {
   public sendForm (): void {
     this.$refs.registerForm.validate((valid) => {
       if (valid) {
-        console.log(this.registerForm)
+        axios.post('/AuthenticateUser/register', this.registerForm).then(
+          () => this.$router.push('/login')
+        )
       }
     })
   }
