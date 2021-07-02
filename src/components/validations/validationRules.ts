5f5c4ab3756b93f26e623177ecee1b99f83f6126
unknown <krystianduda64@gmail.com>
2021-07-02 08:55:12 +0200

    Removed redundant code

--- src/components/validations/validationRules.ts
+++ src/components/validations/validationRules.ts
@@ -1,25 +1,19 @@
 import { baseRule } from '@/components/validations/baseRule'
 
-export const email: baseRule[] = [
-  {
-    required: true,
-    message: 'Email is required',
-    trigger: 'blur'
-  }
-]
+export const email: baseRule = {
+  required: true,
+  message: 'Email is required',
+  trigger: 'blur'
+}
 
-export const password: baseRule[] = [
-  {
-    required: true,
-    message: 'Password is required',
-    trigger: 'blur'
-  }
-]
+export const password: baseRule = {
+  required: true,
+  message: 'Password is required',
+  trigger: 'blur'
+}
 
-export const passwordConfirmation: baseRule[] = [
-  {
-    required: true,
-    message: 'Password confirmation is required',
-    trigger: 'blur'
-  }
-]
+export const passwordConfirmation: baseRule = {
+  required: true,
+  message: 'Password confirmation is required',
+  trigger: 'blur'
+}
