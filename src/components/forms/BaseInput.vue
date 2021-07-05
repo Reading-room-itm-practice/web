8fd4d7729489e0b446537d135d2333f3f614664d
unknown <krystianduda64@gmail.com>
2021-07-05 11:11:54 +0200

    Added base input

---
+++ src/components/forms/BaseInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-input v-model='input' v-on:input='emitBaseInput'></el-input>
+</template>
+
+<script lang='ts'>
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class BaseInput extends Vue {
+  public input = ''
+  @Prop() readonly inputType: string
+
+  public emitBaseInput (): void {
+    this.$emit('base-input', {
+      type: this.inputType,
+      body: this.input
+    })
+  }
+
+  public emitInput (event): void {
+    this.$emit('form-input', event)
+  }
+}
+</script>
