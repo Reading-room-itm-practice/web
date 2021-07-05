5cf7fc383b86fe030a46bcbb79a60c55e9c7d4d2
unknown <krystianduda64@gmail.com>
2021-07-05 14:03:56 +0200

    20-create-middleware-in-router-dir

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,9 @@ import Home from '../views/Home.vue'
 import RegisterPage from '../views/register/RegisterPage.vue'
 import LoginPage from '../views/login/LoginPage.vue'
 import ProfilePage from '../views/profile/ProfilePage.vue'
+import auth from '@/router/middleware/auth'
+import guest from '@/router/middleware/guest'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -16,17 +19,32 @@ const routes: Array<RouteConfig> = [
   {
     path: '/register',
     name: 'RegisterPage',
-    component: RegisterPage
+    component: RegisterPage,
+    meta: {
+      middleware: [
+        guest
+      ]
+    }
   },
   {
     path: '/login',
     name: 'LoginPage',
-    component: LoginPage
+    component: LoginPage,
+    meta: {
+      middleware: [
+        guest
+      ]
+    }
   },
   {
     path: '/profile',
     name: 'ProfilePage',
-    component: ProfilePage
+    component: ProfilePage,
+    meta: {
+      middleware: [
+        auth
+      ]
+    }
   }
 ]
 
@@ -34,6 +52,21 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const middleware = to.meta.middleware
+
+  if (!middleware) {
+    return next()
+  }
+
+  return middleware[0]({
+    to,
+    from,
+    next,
+    store
+  })
 })
 
 export default router
