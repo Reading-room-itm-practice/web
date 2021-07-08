24068240aba84ef0a97d1f200731cb42d71ee681
unknown <krystianduda64@gmail.com>
2021-07-08 14:20:52 +0200

    Added categories page with basic logic

--- src/views/admin/content/AdminCard.vue
+++ src/views/admin/content/AdminCard.vue
@@ -4,16 +4,20 @@
       <el-row>
         <authors></authors>
       </el-row>
+      <el-row>
+        <categories></categories>
+      </el-row>
     </el-col>
   </el-row>
 </template>
 
 <script lang='ts'>
 import { Vue, Component } from 'vue-property-decorator'
-import Authors from '@/components/forms/selects/content/Authors.vue'
+import Authors from '@/components/forms/selects/Authors.vue'
+import Categories from '@/components/forms/selects/Categories.vue'
 
 @Component({
-  components: { Authors }
+  components: { Categories, Authors }
 })
 export default class AdminCard extends Vue {
 }
