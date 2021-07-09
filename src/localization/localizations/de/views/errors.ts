9c5cd456d309aabd21e4d000786ba65ce2e14e93
unknown <krystianduda64@gmail.com>
2021-07-09 14:00:02 +0200

    Added german localization of 404 status response

--- src/localization/localizations/de/views/errors.ts
+++ src/localization/localizations/de/views/errors.ts
@@ -1,4 +1,7 @@
 export const errors = {
+  name: 'error',
   title: 'Verbindungsfehler',
-  networkError: 'Könnte keine Verbindung zum Server herstellen'
+  networkError: 'Könnte keine Verbindung zum Server herstellen',
+  notFound: 'Kann angeforderte Ressource nicht finden',
+  credentials: 'Falsche Anmeldeinformationen'
 }
