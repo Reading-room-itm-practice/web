02b56d0910df254b2fce556ce609e555575ddf7c
unknown <krystianduda64@gmail.com>
2021-07-16 11:58:38 +0200

    53-implement-review-logic

--- src/localization/localizations/de/components/rating.ts
+++
@@ -1,7 +0,0 @@
-export const rating = {
-  stars: {
-    group: 'rating',
-    title: 'Bewertung wurde aktualisiert!',
-    text: 'Ihre Bewertung wurde gespeichert!'
-  }
-}
--- src/localization/localizations/en/components/rating.ts
+++
@@ -1,7 +0,0 @@
-export const rating = {
-  stars: {
-    group: 'rating',
-    title: 'Updated rating!',
-    text: 'Your rating has been saved!'
-  }
-}
