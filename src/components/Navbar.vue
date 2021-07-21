8c98edb25c59ceabfdc8a15b7c331d999f44385c
unknown <krystianduda64@gmail.com>
2021-07-21 13:05:07 +0200

    Updated navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,17 @@
 <template>
-  <el-row :gutter="24" id="nav">
-    <el-col :span="8">
+  <el-row id="nav">
+    <el-col :span="2" :offset='5'>
       <router-link to="/">{{ $t('navbar.home') }}</router-link>
       <br>
       <router-link to="/admin" v-if="getUserRole.includes(UserRoles.ADMIN)">Admin</router-link>
     </el-col>
     <el-col :span="10">
       <el-input v-model="searchInput" :placeholder="$t('navbar.searchBar')">
-        <el-select v-model='selectedResourceType' slot='prepend' placeholder='heklp'>
+        <el-select style='width: 120px;' v-model='selectedResourceType' slot='prepend'>
           <el-option v-for='type in types' :key='type.id' :label='type.name' :value='type.id'></el-option>
         </el-select>
         <el-button @click="search" slot='append'>{{ $t('navbar.searchButton') }}</el-button>
       </el-input>
-    </el-col>
-    <el-col :span="2">
     </el-col>
     <el-col :span="2">
       <div v-if="isLoggedIn">
@@ -45,16 +43,15 @@ export default class Navbar extends Vue {
   private UserRoles = UserRoles
   private types: Array<BasicResource> = [
     { id: '/', name: 'All' },
-    { id: '/Author/', name: 'Authors' },
-    { id: '/Books/', name: 'Books' },
-    { id: '/Categories/', name: 'Categories' },
-    { id: '/User/', name: 'Users' }
+    { id: '/Author', name: 'Authors' },
+    { id: '/Books', name: 'Books' },
+    { id: '/Categories', name: 'Categories' },
+    { id: '/User', name: 'Users' }
   ]
 
   private selectedResourceType: string = this.types[0].id
 
   private search (): void {
-    console.log(`searching for ${this.searchInput} ${this.selectedResourceType}`)
     if (this.searchInput && this.selectedResourceType) {
       this.$router.push({
         name: 'SearchPage',
