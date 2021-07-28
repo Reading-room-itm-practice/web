f72a00c8568cdb712e554d2fe7971bf8dcb6f573
unknown <krystianduda64@gmail.com>
2021-07-28 13:04:10 +0200

    Moved contents of suggestion page to suggestion card

--- src/views/suggestions/SuggestionPage.vue
+++ src/views/suggestions/SuggestionPage.vue
@@ -1,38 +1,20 @@
 <template>
   <el-col :offset='8'>
     <el-row>
-      <el-tabs v-model="activeTab" @tab-click="changeTab">
-        <el-tab-pane label="Books" name="books">
-          <span slot="label"><el-image :src="require('@/assets/book.png')" class='tab-image'></el-image></span>
-          <suggested-books></suggested-books>
-        </el-tab-pane>
-        <el-tab-pane label="Categories" name="categories">
-          <span slot="label"><el-image :src="require('@/assets/text.png')" class='tab-image'></el-image></span>
-          Cats
-        </el-tab-pane>
-        <el-tab-pane label="Authors" name="authors">
-          <span slot="label"><el-image :src="require('@/assets/man.png')" class='tab-image'></el-image></span>
-          Authors
-        </el-tab-pane>
-      </el-tabs>
+      <suggestion-card></suggestion-card>
     </el-row>
   </el-col>
 </template>
 
 <script lang='ts'>
 import { Vue, Component } from 'vue-property-decorator'
-import SuggestedBooks from '@/views/suggestions/content/SuggestedBooks.vue'
+import SuggestionCard from '@/views/suggestions/content/SuggestionCard.vue'
 
 @Component({
   components: {
-    SuggestedBooks
+    SuggestionCard
   }
 })
 export default class SuggestionPage extends Vue {
-  private activeTab = 'books'
-
-  changeTab (tab: Vue): void {
-    this.activeTab = tab.name
-  }
 }
 </script>
---
+++ src/views/suggestions/content/SuggestionCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-tabs v-model="activeTab" @tab-click="changeTab">
+    <el-tab-pane label="Books" name="books">
+      <span slot="label"><el-image :src="require('@/assets/book.png')" class='tab-image'></el-image></span>
+      <suggested-books></suggested-books>
+    </el-tab-pane>
+    <el-tab-pane label="Categories" name="categories">
+      <span slot="label"><el-image :src="require('@/assets/text.png')" class='tab-image'></el-image></span>
+      Cats
+    </el-tab-pane>
+    <el-tab-pane label="Authors" name="authors">
+      <span slot="label"><el-image :src="require('@/assets/man.png')" class='tab-image'></el-image></span>
+      Authors
+    </el-tab-pane>
+  </el-tabs>
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator'
+import SuggestedBooks from '@/views/suggestions/content/SuggestedBooks.vue'
+
+@Component({
+  components: {
+    SuggestedBooks
+  }
+})
+export default class SuggestionCard extends Vue {
+  private activeTab = 'books'
+
+  changeTab (tab: Vue): void {
+    this.activeTab = tab.name
+  }
+}
+</script>
