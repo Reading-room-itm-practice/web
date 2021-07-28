470c9d2c707aa1484813614be0c1804bf7a706cf
unknown <krystianduda64@gmail.com>
2021-07-28 13:01:14 +0200

    Added images instead of strings in tabs

--- src/views/suggestions/SuggestionPage.vue
+++ src/views/suggestions/SuggestionPage.vue
@@ -3,10 +3,16 @@
     <el-row>
       <el-tabs v-model="activeTab" @tab-click="changeTab">
         <el-tab-pane label="Books" name="books">
+          <span slot="label"><el-image :src="require('@/assets/book.png')" class='tab-image'></el-image></span>
           <suggested-books></suggested-books>
         </el-tab-pane>
-        <el-tab-pane label="Books2" name="books2">
-          asd
+        <el-tab-pane label="Categories" name="categories">
+          <span slot="label"><el-image :src="require('@/assets/text.png')" class='tab-image'></el-image></span>
+          Cats
+        </el-tab-pane>
+        <el-tab-pane label="Authors" name="authors">
+          <span slot="label"><el-image :src="require('@/assets/man.png')" class='tab-image'></el-image></span>
+          Authors
         </el-tab-pane>
       </el-tabs>
     </el-row>
