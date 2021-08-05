c0ec03a54269bfec908ec8bec6b6f1ab693acda1
Bon3z <krystianduda64@gmail.com>
2021-08-05 13:41:57 +0200

    Added types to args&project rename

--- src/router/middleware/auth.ts
+++ src/router/middleware/auth.ts
@@ -1,6 +1,7 @@
 import store from '@/store'
+import { RouteMeta } from 'vue-router'
 
-export default function auth ({ next }): void {
+export default function auth ({ next }: RouteMeta): void {
   if (!store.getters.isLoggedIn) {
     return next({
       name: 'LoginPage'
